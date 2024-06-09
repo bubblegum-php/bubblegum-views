37436ed247328b401c7337f1adab93316308b5e7
Oleksii Fedorenko <lankrf@gmail.com>
2024-06-09 13:36:11 +0300

    Init View and composer.json

---
+++ src/Views/View.php
@@ -0,0 +1,25 @@
+<?php
+
+
+use Bubblegum\Request;
+use Bubblegum\Routes\RoutedComponent;
+
+class View extends RoutedComponent
+{
+    protected static string $path = ROOT_PATH . 'app/Views/';
+    function content(Request $request, array $data = []): string
+    {
+        $path = self::$path.str_replace('.', DIRECTORY_SEPARATOR, $this->destinationName).'.php';
+        return self::getContentFromFile($path, $request, $data);
+    }
+
+    protected static function getContentFromFile(string $filePath, Request $request, array $data): string
+    {
+        extract(['request' => $request->all(), 'data' => $data], EXTR_SKIP);
+        ob_start();
+        require $filePath;
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    }
+}
